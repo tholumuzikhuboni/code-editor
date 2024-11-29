c80ac331f5897b63d233dfc27f8f152eb1c47dd8
Tholumuzi Khuboni <contact@tholumuzi.co.za>
2024-11-29 17:45:02 +0200

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,12 @@
 import { loginUser } from "./auth.js";
 
 document.getElementById("loginForm").addEventListener("submit", (e) => {
-    e.preventDefault(); // Prevent form from refreshing page
+    e.preventDefault(); // Prevent form default behavior
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
-    loginUser(email, password); // Call loginUser function
+    
+    console.log("Attempting login with:", email, password); //
+
+    // Call the login function
+    loginUser(email, password);
 });
