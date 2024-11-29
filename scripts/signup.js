e5640633579df6e8450833c515e6b233c1551342
Tholumuzi Khuboni <contact@tholumuzi.co.za>
2024-11-29 17:31:14 +0200

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,8 +1,8 @@
 import { registerUser } from "./auth.js";
 
 document.getElementById("signupForm").addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevent form from refreshing page
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
-    registerUser(email, password);
+    registerUser(email, password); // Call registerUser function
 });
