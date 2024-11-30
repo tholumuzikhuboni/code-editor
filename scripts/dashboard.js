0f8e1bf6485fb4fd5d72dd37b8c3433fe16db49b
Tholumuzi Khuboni <contact@tholumuzi.co.za>
2024-11-30 07:25:20 +0200

    Update dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,7 +2,11 @@ import { checkUserState, logoutUser } from "./auth.js";
 
 // Check if the user is logged in
 checkUserState((user) => {
-    document.getElementById("welcomeMessage").textContent = `Welcome, ${user.email}`;
+    // Get the name or fallback to email
+    const userName = user.displayName || user.email;
+    
+    // Update the welcome message
+    document.getElementById("welcomeMessage").textContent = `Welcome, ${userName}`;
 });
 
 // Logout button
